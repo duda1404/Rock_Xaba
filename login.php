fd7eb044a1f6bb200c4a65ca67a51a32df1924b3
duda1404 <87455625+duda1404@users.noreply.github.com>
2022-05-28 21:18:52 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+include 'header.php'
+?>
+
+   <!-- Seção de Login, contendo o formulário a ser preenchido, sendo esperado usuário e senha a ser logado-->
+
+     <div class="right-login">
+       <div class="card-login">
+        <h1>Login</h1>
+         <div class="textfield">
+           <label for="user">Usuário</label>
+           <input type="text" name="user" placeholder="Usuário">
+         </div>
+
+         <div class="textfield">
+          <label for="senha">Senha</label>
+          <input type="password" name="password" placeholder="Senha">
+        </div>
+
+        <button class="btn-login"></button>
+
+       </div>
+     </div>
+
+</body>
+</html>
